dac4de20822f8ac24ab2d48daddd8ed611b38ac5
Ryan Logan <ryan.k.logan@gmail.com>
2019-03-06 15:25:54 -0500

    Added OnClick Functionality to RecyclerView Adapter

--- app/src/main/java/com/mcgill/ecse428/foodme/adapters/RestaurantAdapter.java
+++ app/src/main/java/com/mcgill/ecse428/foodme/adapters/RestaurantAdapter.java
@@ -1,9 +1,11 @@
 package com.mcgill.ecse428.foodme.adapters;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.mcgill.ecse428.foodme.R;
 import com.mcgill.ecse428.foodme.model.Restaurant;
@@ -19,9 +21,12 @@ import androidx.recyclerview.widget.RecyclerView;
 public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.MyViewHolder> {
 
     private List<Restaurant> restaurantList;
+    private Context mContext;
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public static class MyViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener, View.OnLongClickListener{
         protected TextView name, cuisine, price, distance;
+        private ItemClickListener clickListener;
 
         public MyViewHolder(View view) {
             super(view);
@@ -29,8 +34,20 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.My
             cuisine = view.findViewById(R.id.findRestaurantCuisine);
             distance = view.findViewById(R.id.findRestaurantDistance);
             price = view.findViewById(R.id.findRestaurantPrice);
+            view.setOnClickListener(this);
+            view.setOnLongClickListener(this);
+        }
 
+        public void setClickListener(ItemClickListener itemClickListener){
+            this.clickListener = itemClickListener;
+        }
 
+        public void onClick(View view){
+            clickListener.onClick(view, getLayoutPosition(),false);
+        }
+        public boolean onLongClick(View view){
+            clickListener.onClick(view, getLayoutPosition(),true);
+            return true;
         }
     }
 
@@ -41,7 +58,8 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.My
 
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_restaurant, parent, false);
+        this.mContext = parent.getContext();
+        View itemView = LayoutInflater.from(mContext).inflate(R.layout.recycler_restaurant, parent, false);
 
 
         return new MyViewHolder(itemView);
@@ -55,6 +73,19 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.My
         holder.cuisine.setText(restaurant.getCuisine());
         holder.distance.setText(restaurant.getDistance());
 
+        holder.setClickListener(new ItemClickListener(){
+           @Override
+           public void onClick(View view, int position, boolean isLongClick){
+               List<Restaurant> rList = restaurantList;
+               if(isLongClick){
+                   Toast.makeText(mContext, "#" + position +" - "+rList.get(position).getName()
+                        +" (Long click)", Toast.LENGTH_SHORT).show();
+               }
+               else{
+                   Toast.makeText(mContext,"#"+position+" - "+rList.get(position).getName(),Toast.LENGTH_SHORT).show();
+               }
+           }
+        });
 
     }
 
@@ -64,5 +95,8 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.My
         return restaurantList.size();
     }
 
+    public interface ItemClickListener{
+        void onClick (View view, int position, boolean isLongClick);
+    }
 
 }
